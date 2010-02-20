505547f082f4351ccb0facb3db8da44ee583bb6c
Ryan Kabir <ryan@causes.com>
2010-02-20 01:57:18 -0800

    it lives

---
+++ init_ryborg.rb
@@ -0,0 +1,3 @@
+class String
+  include Ryborg::String
+end
---
+++ string.rb
@@ -0,0 +1,13 @@
+module Ryborg::String
+    def scrub
+      out = self.downcase
+      out = out.strip
+      while out =~ /\w*\W$/
+        out.chop!
+      end
+      while out =~ /^\W\w+/
+        out.slice!(0,1)
+      end
+      return out
+    end
+end
